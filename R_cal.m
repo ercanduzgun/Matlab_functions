a43fda20f1cb2df2241342f0f5b39ad63c7b7bfc
Ercan Duzgun <eduzgun@uludag.edu.tr>
2023-08-04 10:02:01 +0300

    alpha_beta_gamma_to_R_rotationMatrix
    alpha_beta_gamma_to_R_rotationMatrix

---
+++ R_cal.m
@@ -0,0 +1,13 @@
+function [rotation] = R_cal(alpha,beta,gamma)
+Rx=[1   0               0;
+    0   cos(alpha)  -sin(alpha)
+    0   sin(alpha)  cos(alpha)];
+Ry=[cos(beta)   0   sin(beta)
+    0           1       0
+    -sin(beta) 0   cos(beta)];
+Rz=[cos(gamma)  -sin(gamma)   0
+    sin(gamma)  cos(gamma)    0
+    0           0           1];
+rotation=Rz*Ry*Rx;
+end
+
